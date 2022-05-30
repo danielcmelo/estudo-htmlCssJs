f8f05ae625d5312db4cd5713be3cf315b752310b
Daniel Melo <dcm189497@gmail.com>
2022-05-30 14:46:42 +0100

    output

---
+++ output/script.js
@@ -0,0 +1,7 @@
+document.getElementById('titulo').innerHTML = 'Obrigado'; //Manipulando um elemento html
+
+document.write('De nada');//Criando seu próprio elemento
+
+alert('Não seja por isso');//Utilizando a janela do navegador
+
+console.log('Ihaa')//Pelo console
