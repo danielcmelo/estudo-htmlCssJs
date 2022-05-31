596f549fd666373df4bb46193dc31637e0f8eedf
Daniel Melo <dcm189497@gmail.com>
2022-05-31 12:38:56 +0100

    Event click

---
+++ eventoDeClick/script.js
@@ -0,0 +1,7 @@
+// function clicou(){
+//     alert('Voce clicou no botao')
+// }
+
+// function clicou() {
+//     document.getElementById('titulo').innerHTML = 'Voce clicou e o texto mudou';
+// }
