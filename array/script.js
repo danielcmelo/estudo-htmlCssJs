7242a63d7da8e91bfe2d8505c2d9fb61143384d6
Daniel Melo <dcm189497@gmail.com>
2022-05-31 16:53:27 +0100

    module array

---
+++ array/script.js
@@ -0,0 +1,21 @@
+// Sem array
+
+let carro1 = 'Palio';
+let carro2 = 'Uno';
+let carro3 = 'Corolla';
+let carro4 = 'Ferrari';
+
+// Com array
+
+let carros = [
+    'Palio',
+    'Uno',
+    'Corolla',
+    'Ferrari'
+];
+
+console.log(carros);
+console.log(carros[4]);
+console.log(carros[0],carros[1]);
+carros.find
+console.log(carros)
