d8ba1d5e1bf86664274b01c2a77e9e016425ac86
Daniel Melo <dcm189497@gmail.com>
2022-06-01 12:09:33 +0100

    menu toggle

--- object/script.js
+++ object/script.js
@@ -1,8 +1,20 @@
 // Objeto com 3 propriedades
-let carros = {
-    nome:'Fiat',
-    modelo:'Uno',
-    peso:'800kg'
-};
+// let carros = {
+//     nome:'Fiat',
+//     modelo:'Uno',
+//     peso:'800kg'
+// };
 
-console.log(carros.modelo)
+// console.log(carros.modelo)
+
+// Objeto dentro de array
+let carros = [
+    {nome: 'Fiat', modelo: 'Uno'},
+    {nome: 'Fiat', modelo: 'Palio'},
+    {nome: 'Toyota', modelo: 'Corolla'},
+    {nome: 'Ferrari', modelo: 'Spider'}
+];
+
+console.log(carros);
+console.log(carros[3]);
+console.log(carros[3].nome);
