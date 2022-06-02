ad16407174c37b9258fc79b66682b344e8d18aa7
Daniel Melo <dcm189497@gmail.com>
2022-06-02 15:45:19 +0100

    module funtion

---
+++ function/script.js
@@ -0,0 +1,44 @@
+// function alterar(){
+//     document.getElementById('titulo').innerHTML = 'Teste 123';
+//     document.getElementById('campo').value = 'trocou o campo'
+// }
+
+// alterar()
+
+//função com parâmetros
+// function alterar(titulo){
+//         document.getElementById('titulo').innerHTML = titulo;
+//         document.getElementById('campo').value = titulo;
+// }
+
+// alterar('12 12 teste')
+
+// function somar(x, y){
+//     let total = x + y;
+//     document.getElementById('campo').value = total;
+// }
+
+// somar(2, 3)
+
+
+
+//function com return
+function somar(x, y){
+    let total = x + y;
+    
+    return total;
+}
+
+let resultado = somar(10, 31)
+console.log(resultado)
+
+
+
+
+
+
+
+
+
+
+
