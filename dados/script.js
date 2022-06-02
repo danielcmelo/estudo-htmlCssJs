86e01ba7886143105b44062e893f59b448411ce3
Daniel Melo <dcm189497@gmail.com>
2022-06-02 15:18:37 +0100

    module dados

---
+++ dados/script.js
@@ -0,0 +1,9 @@
+var nome = 'Daniel';//String
+var idade = 50;//Number
+var salvo = true;//Boolean
+var array = [];//Array
+var obj = {};//Object
+var cidade;//Undefined
+
+var tipo = typeof nome;
+console.log(tipo)
